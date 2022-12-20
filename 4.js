f6d1186e7e9bee1751e7737f4698aae1d8e39321
Osaki <105201941+Paul3831@users.noreply.github.com>
2022-12-20 11:00:33 +0100

    Delete 4.js

--- 4.js
+++
@@ -1,9 +0,0 @@
-//- 4 Écrivez une fonction qui prend un tableau de chaînes de caractères et renvoie un tableau de chaînes de caractères triées par ordre alphabétique.
-
-//Les lettres dans le désordre
-const lettres = ['z', 'c', 'a', 'h'];
-// Triage des lettres
-lettres.sort();
-// Sortie console
-console.log("Classement par ordre Alphabétique :");
-console.log(lettres);//['a', 'c', 'h', 'z']
