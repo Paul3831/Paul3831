ddff8458dc8fadabc251d0b697484fc932421d80
Osaki <105201941+Paul3831@users.noreply.github.com>
2022-12-20 10:59:46 +0100

    Add files via upload

---
+++ 10.js
@@ -0,0 +1,12 @@
+//- 10 Écrivez une fonction qui prend une chaîne de caractères et renvoie une nouvelle chaîne avec toutes les consonnes en majuscules.
+
+const C = "Toutes les consonnes en majuscules";
+MP = C.toUpperCase(); //passage des consonnes en majuscules
+
+MP = MP.replaceAll("A", "a");
+MP = MP.replaceAll("E", "e");
+MP = MP.replaceAll("U", "u");
+MP = MP.replaceAll("I", "i");
+MP = MP.replaceAll("O", "o");
+MP = MP.replaceAll("Y", "y");
+console.log(MP);//TouTeS LeS CoNSoNNeS eN MaJuSCuLeS
