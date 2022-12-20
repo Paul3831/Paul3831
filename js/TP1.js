d6e73b0b3db33d35e55f480be2117a0ce4e690d7
Osaki <105201941+Paul3831@users.noreply.github.com>
2022-12-20 10:57:53 +0100

    Add files via upload

--- js/TP1.js
+++ js/TP1.js
@@ -1,9 +1,12 @@
+//- 1 Écrivez une fonction qui prend deux nombres comme arguments et renvoie leur somme.
+
+//- 2 Écrivez une fonction qui prend un tableau de nombres et renvoie le plus grand nombre du tableau.
+
 //- 3 Écrivez une fonction qui prend une chaîne de caractères et renvoie une nouvelle chaîne de caractères dont toutes les voyelles ont été supprimées.
-let string = "azerqtsfdyucbgstwioptnblmp";
 
-string;
+//- 4 Écrivez une fonction qui prend un tableau de chaînes de caractères et renvoie un tableau de chaînes de caractères triées par ordre alphabétique.
 
- // MATH - 5 Écrivez une fonction qui prend un nombre en argument et renvoie une chaîne de caractères représentant ce nombre en mots. Par exemple, si l'entrée est 42, la fonction doit retourner "quarante-deux".
+//- 5 Écrivez une fonction qui prend un nombre en argument et renvoie une chaîne de caractères représentant ce nombre en mots. Par exemple, si l'entrée est 42, la fonction doit retourner "quarante-deux".
 
 //- 6 Écrivez une fonction qui prend un tableau d'objets et un nom de propriété et renvoie un tableau des valeurs de cette propriété dans chaque objet.
 
