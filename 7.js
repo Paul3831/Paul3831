5f9501ecaf10f0ca3ab38995f1f9c01e71f51def
Osaki <105201941+Paul3831@users.noreply.github.com>
2022-12-20 11:00:41 +0100

    Delete 7.js

--- 7.js
+++
@@ -1,6 +0,0 @@
-//- 7 Écrivez une fonction qui prend un tableau de nombres et renvoie un tableau de nombres triés par ordre décroissant.
-
-const numbers = [13,8,2,21,5,1,3,1];
-const byValue = (a,b) => a - b;
-const sorted = [...numbers].sort(byValue);
-console.log(sorted); // [1,1,2,3,5,8,13,21]
