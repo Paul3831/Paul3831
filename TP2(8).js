0ab5fe2a793968cc228853a8ff956d9d72e0c609
Osaki <105201941+Paul3831@users.noreply.github.com>
2022-12-22 16:52:37 +0100

    Delete TP2(8).js

--- TP2(8).js
+++
@@ -1,4 +0,0 @@
-//Écrivez un programme Node.js qui utilise le module path pour manipuler les chemins de fichiers.
-
-//module path : Chemin d’accès permet d’utiliser les répertoires et les chemins d’accès aux fichiers.
-
